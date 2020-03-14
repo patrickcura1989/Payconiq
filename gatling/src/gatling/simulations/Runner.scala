83f80f2c34448355e1069a142fe0a551abcb72b7
patrickcura1989 <Airnewzealand123!>
2020-03-14 17:30:10 +1300

    Upgraded gatling

--- gatling/src/gatling/simulations/Runner.scala
+++ gatling/src/gatling/simulations/Runner.scala
@@ -3,12 +3,12 @@ import io.gatling.core.Predef._
 import scala.concurrent.duration._
 
 class Runner extends Simulation {
-  val httpConf = http.baseURL("https://api.github.com/")
+  val httpConf = http.baseUrl("https://api.github.com/")
   val scn = scenario("Get All Gists")
     .exec(http("Get All Gists")
       .get("users/patrickcura1989/gists").header("User-Agent",""))
   setUp(
-    scn.inject(rampUsers(1) over (5 seconds),
+    scn.inject(rampUsers(1) during (5 seconds),
       constantUsersPerSec(1) during (10 seconds))
   ).protocols(httpConf)
 }
