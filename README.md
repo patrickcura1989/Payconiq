fb964201df426ba749889165255aa511aeed2074
patrickcura1989 <Airnewzealand123!>
2020-03-14 17:20:43 +1300

    Edited gatling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-> Usage
+> Running JUnit Tests
 - Please set accessToken in you environment variables to run the tests
+
+> Running Gatling Performance/Load Tests
+- gradlew gatlingRun-Runner
---
+++ gatling/src/gatling/scala/Runner.scala
@@ -0,0 +1,14 @@
+import io.gatling.http.Predef._
+import io.gatling.core.Predef._
+import scala.concurrent.duration._
+
+class Runner extends Simulation {
+  val httpConf = http.baseURL("https://api.github.com/")
+  val scn = scenario("Get All Gists")
+    .exec(http("Get All Gists")
+      .get("users/patrickcura1989/gists").header("User-Agent",""))
+  setUp(
+    scn.inject(rampUsers(1) over (5 seconds),
+      constantUsersPerSec(1) during (10 seconds))
+  ).protocols(httpConf)
+}
